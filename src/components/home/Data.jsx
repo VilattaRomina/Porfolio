203b6b7b7a80aba039c4065b1c445673bc7586b8
VilattaRomina <vilattaromina@gmail.com>
2022-12-26 17:57:57 -0300

    data home

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Data = () => {
+    return (
+        <div className='home__data'>
+            <h1 className='home__title'> Hola, soy Romi</h1>
+            <h3 className='home__subtitle'> Frontend Developer</h3>
+            <p className='home__description'>Soy estudiante de la carrera 
+Certified Tech Developer. 
+Responsable, comprometida y con 
+gran capacidad de comunicación e 
+iniciativa. .</p>
+
+            <a href='#contact' className='button button--flex'> Contácteme
+            <i class="uil uil-message"></i> </a>
+        </div>
+
+    );
+}
+
+export default Data;
