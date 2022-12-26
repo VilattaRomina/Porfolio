e0b0a3ae7e2a5ffd5202a69227bbbafd5fb657d1
VilattaRomina <vilattaromina@gmail.com>
2022-12-26 17:57:44 -0300

    componenete skills

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Backend = () => {
+  return (
+    <div className='skills__content'>
+        <h3 className='skills__title'>Backend</h3>
+
+        <div className='skills__box'>
+            <div className='skills__group'>
+                <div className='skills__data'>
+                <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className='skills__name'>Java</h3>
+                        
+                    </div>
+                </div>
+                <div className='skills__data'>
+                <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className='skills__name'>MySQL</h3>
+                        
+                    </div>
+                </div>
+                <div className='skills__data'>
+                <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className='skills__name'>MongoDB</h3>
+                        
+                    </div>
+                </div>
+                <div className='skills__data'>
+                <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className='skills__name'>POO</h3>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Backend;
